6daa37cb2d87d728a74617931f15caf46e7dd72e
4ura <4uralynn@protonmail.com>
2022-03-03 13:19:40 -0800

    Add initial files for file_summary script.

---
+++ script/csv_2_html.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import sys
+import csv
+import os
+
+
+def process_csv(csv_file):
+    """Turn the contents of the CSV file into a list of lists"""
+    print("Processing {}".format(csv_file))
+    with open(csv_file, "r") as datafile:
+        data = list(csv.reader(datafile))
+    return data
+
+
+def data_to_html(title, data):
+    """Turns a list of lists into an HTML table"""
+    # HTML Headers
+    html_content = """
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<html lang="ru">
+<head>
+<style>
+table {
+  width: 25%;
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+}
+
+tr:nth-child(odd) {
+  background-color: #dddddd;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+</style>
+</head>
+<body>
+"""
+
+    # Add the header part with the given title
+    html_content += "<h2>{}</h2><table>".format(title)
+
+    # Add each row in data as a row in the table
+    # The first line is special and gets treated separately
+    for i, row in enumerate(data):
+        html_content += "<tr>"
+        for column in row:
+            if i == 0:
+                html_content += "<th>{}</th>".format(column)
+            else:
+                html_content += "<td>{}</td>".format(column)
+        html_content += "</tr>"
+
+    html_content += """</tr></table></body></html>"""
+    return html_content
+
+
+def write_html_file(html_string, html_file):
+
+    # Making a note of whether the html file we're writing exists or not
+    if os.path.exists(html_file):
+        print("{} already exists. Overwriting...".format(html_file))
+
+    with open(html_file, 'w') as htmlfile:
+        htmlfile.write(html_string)
+    print("Table succesfully written to {}".format(html_file))
+
+
+def create(csv_file, html_file):
+    """Verifies the arguments and then calls the processing function"""
+    # Process the data and turn it into an HTML
+    data = process_csv(csv_file)
+    title = os.path.splitext(os.path.basename(csv_file))[
+        0].replace("_", " ").title()
+    print("Populating {} data into HTML format...".format(csv_file))
+    html_string = data_to_html(title, data)
+    write_html_file(html_string, html_file)
+
+
---
+++ script/file_summary.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import re
+import subprocess
+import signal
+import shutil
+import time
+import csv
+from csv_2_html import create
+from alive_progress import alive_bar
+
+#Function to open a file by type
+def parse_dir(path, agent_list, linecount):
+    success = 0
+    linecount.append(0)
+    for file in os.listdir(path):
+        fullpath = os.path.join(path, file)
+        csv_file = re.search(r"^.*\.csv$", file)
+        pdf_file = re.search(r"\.pdf", file)
+        if csv_file is not None:
+            #print(file)
+            linecount[0] += process_csv(file, fullpath, agent_list)
+            success += 1
+        yield 
+    if success == 0:
+        print("There are no files that can be processed.")
+        sys.exit(1)
+        return 1
+    return 0
+
+def process_csv(filename, path, agent_list):
+    count = 0
+    with open(path, 'r') as csvfile:
+        for line in csvfile.readlines():
+            count += 1
+            results = re.findall(r"[\u0400-\u04FF ]*", line)
+            for agent in results:
+                if agent.strip() == "":
+                    continue
+                else:
+                    if agent.lower().strip() not in agent_list:
+                        agent_list[agent.lower().strip()] = {}
+                    if agent.lower().strip() in agent_list:
+                        if filename not in agent_list[agent.lower().strip()]:
+                            agent_list[agent.lower().strip()][filename] = []
+                        if filename in agent_list[agent.lower().strip()]:
+                            agent_list[agent.lower().strip()][filename].append(count)
+    csvfile.close()
+    return count
+                
+def write_csv(agent_list):
+    mostoccur = str
+
+#    trans_limit = False
+
+    with open('referenced.csv', "w", newline="") as newcsv:
+        newcsv.write("Agency/Reference,Files,Lines,File with Highest Occurrences\n")
+        for agent in agent_list:
+            countfiles = 0
+            countlines = 0
+            mo_count = 0
+
+#            final = agent
+#            if not trans_limit:
+#                final = blurb_translate(agent)
+#                if final == agent:
+#                    trans_limit = True
+
+            newcsv.write(str(agent) + ",")      #for translation above algorthim, change 'agent' argument to 'final' 
+            for filename in agent_list[agent]:
+                countfiles += 1
+                for index in agent_list[agent][filename]:
+                    countlines += 1
+                if mo_count < countlines:
+                    mostoccur = filename
+            newcsv.write(str(countfiles) + "," +  str(countlines) + "," + str(mostoccur) + "\n")
+    newcsv.close()
+
+#def blurb_translate(agent):
+
+#    working = subprocess.check_output(["trans", "-brief", agent])
+#    final = working.decode('UTF-8').strip()
+#    if final == "":
+#            return agent
+#    else:
+#        return final
+
+
+def parsing_progress(targetpath, agent_list):
+    linecount = []
+    thing = len(os.listdir(targetpath))
+    print("\nGoing through every file in the directory...\n")
+    with alive_bar(thing) as bar:
+        for nextfile in parse_dir(targetpath, agent_list, linecount):
+            bar()
+    print("\nPreparing CSV summary of blurbs from {} files, with {} lines in total.".format(str(thing), str(linecount[0])))
+            
+def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    targetpath = os.path.join(os.path.dirname(os.path.abspath("")), "data", "nsiVSRZ/daily") ##Add final location subdirectories below 'data'
+    agent_list = {}
+    parsing_progress(targetpath, agent_list)
+    #print(agent_list.keys())
+    #blurb_translate(agent_list)
+    write_csv(agent_list)
+    create('referenced.csv', 'referenced.html')
+main()
